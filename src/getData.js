01cc1ba3cac586609cd4cbd33f4040bffaa38b6d
LonelyDaoist <ghabaroughassen@gmail.com>
2020-03-07 10:06:05 +0100

    getData: general infos

--- src/getData.js
+++ src/getData.js
@@ -17,11 +17,17 @@ module.exports = {
 			const info = await page.evaluate(() => {
 				const summary = document.querySelectorAll('div.Summary__Text-sc-1wkzvu-6');
 				const description = document.querySelector('div.ShowMoreText__UITextContainer-sc-5ggbbc-0 p');
+				const general = document.querySelectorAll('li.GeneralList__Item-sc-9gtpjm-1');
+				let general_infos = {};
+				for (let i=0;i<general.length;i++) {
+					general_infos[`info_${i+1}`] = general[i].innerText;
+				}
 				return {
 					type: summary[0].innerText,
 					location: summary[1].innerText,
 					price: summary[2].innerText,
-					description: description.innerText
+					description: description.innerText,
+					...general_infos
 				};
 			});
 			
