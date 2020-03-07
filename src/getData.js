274731257327c6558d83c5baf5616efb30bd5fe5
LonelyDaoist <ghabaroughassen@gmail.com>
2020-03-07 09:58:31 +0100

    getData: description

--- src/getData.js
+++ src/getData.js
@@ -16,10 +16,12 @@ module.exports = {
 			
 			const info = await page.evaluate(() => {
 				const summary = document.querySelectorAll('div.Summary__Text-sc-1wkzvu-6');
+				const description = document.querySelector('div.ShowMoreText__UITextContainer-sc-5ggbbc-0 p');
 				return {
 					type: summary[0].innerText,
 					location: summary[1].innerText,
-					price: summary[2].innerText
+					price: summary[2].innerText,
+					description: description.innerText
 				};
 			});
 			
