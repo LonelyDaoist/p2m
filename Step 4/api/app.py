10bf21a3f5d9238906de69f3b69028e4d6ecf2c2
LonelyDaoist <ghabaroughassen@gmail.com>
2020-05-29 00:50:44 +0100

    mvp

--- Step 4/api/app.py
+++ Step 4/api/app.py
@@ -2,41 +2,45 @@ import flask
 import pickle
 import pandas as pd
 
-with open("Model/rf_model.pickle","rb") as f:       # Loading the model
+with open("Model/rf_model.pickle","rb") as f:           # Loading the model
     model = pickle.load(f)
 
-with open("files/model_inputs.txt","r") as f:       # Loading the model columns
-    cols = f.read().splitlines()                    # which will be used to complete missing values
+with open("files/model_inputs.txt","r") as f:           # Loading the model columns
+    cols = f.read().splitlines()                        # which will be used to complete missing values
 
-with open("files/numeric_inputs.txt","r") as f:     # Loading the columns that needs to be numeric
+with open("files/numeric_inputs.txt","r") as f:         # Loading the columns that needs to be numeric
     numeric_cols = f.read().splitlines()
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__,template_folder="Templates")
 
 #numeric_cols = ["room","bedroom","swimming_pool","area(m�)"]
 
-@app.route("/",methods=["POST"])
+@app.route("/",methods=["GET","POST"])
 def main():
-    data = flask.request.form.to_dict(flat=False)   # Extracting form data and
-                                                    # converting it to a dictionnary 
+    if flask.request.method == "GET":
+        return flask.render_template("predictit.html")  # Display main page
 
-    query = pd.DataFrame(data)                      # Converting the dictionnary into a dataframe
+    if flask.request.method == "POST":
+        data = flask.request.json                       # Extracting the data
+    
+    query = pd.json_normalize(data)                     # Converting json into a dataframe
 
-    for col in query.columns:                       # Converting to numeric columns
+    for col in query.columns:                           # Converting to numeric columns
         if col in numeric_cols:
             query[col] = pd.to_numeric(query[col])
 
-    query = pd.get_dummies(query,prefix='', prefix_sep='')  # Applying one hot encoding to match the model
+    query = pd.get_dummies(query,prefix='',
+                                prefix_sep='')          # Applying one hot encoding to match the model
 
-    for col in cols:                                # If there are missing values
-        if col not in query.columns:                # they will be filled with 0
+    for col in cols:                                    # If there are missing values
+        if col not in query.columns:                    # they will be filled with 0
             query[col] = 0
 
-    prediction = 10 ** model.predict(query)[0]      # Prediction the result
-                                                    # which is initially an ndarray
-                                                    # thus the use of [0]
-
-    return flask.jsonify({"prediction":prediction})
+    prediction = 10 ** model.predict(query)[0]          # Prediction the result
+                                                        # which is initially an ndarray
+                                                        # thus the use of [0]
+    print(prediction)
+    return flask.jsonify({"prediction":prediction})     # Return prediction in a json format
 
 if (__name__=="__main__"):
     app.run()
---
+++ Step 4/api/static/index.js
@@ -0,0 +1,39 @@
+
+// this is for filling the city select box
+let city_select = document.getElementById("city-select");
+
+// put the cities names in this array
+let cities = ["a","b","c","d"];
+
+for (let city of cities) {
+    let option = document.createElement("option");
+    option.text = city;
+    city_select.add(option); 
+}
+// ============================================================================
+
+// this is for sending the request to the server
+async function submit() {
+  /* 
+    let data = {};
+    let inputs = document.getElementsByClassName("form-control");
+    for (input of inputss) {
+      datas[input.id] = input.value;
+    }
+ */
+    let data = {};
+    let inputs = ["category","area(m�)","room","bedroom","swimming_pool"]
+    for (let input of inputs) {
+        let val = document.getElementById(input).value;
+        data[input] = val;
+    }
+    fetch("/", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(response => document.getElementById("prediction").innerHTML = response.prediction);
+}
