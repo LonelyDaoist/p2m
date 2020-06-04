f506948adba1223d420775735cad26b2f3a075d4
LonelyDaoist <ghabaroughassen@gmail.com>
2020-06-04 17:36:33 +0100

    deployed

--- Step 4/api/app.py
+++ Step 4/api/app.py
@@ -1,6 +1,7 @@
 import flask
 import pickle
 import pandas as pd
+from math import exp
 
 with open("Model/rf_model.pickle","rb") as f:           # Loading the model
     model = pickle.load(f)
@@ -24,22 +25,22 @@ def main():
         data = flask.request.json                       # Extracting the data
     
     query = pd.json_normalize(data)                     # Converting json into a dataframe
-
+ 
     for col in query.columns:                           # Converting to numeric columns
         if col in numeric_cols:
             query[col] = pd.to_numeric(query[col])
 
     query = pd.get_dummies(query,prefix='',
                                 prefix_sep='')          # Applying one hot encoding to match the model
-
+   
     for col in cols:                                    # If there are missing values
         if col not in query.columns:                    # they will be filled with 0
             query[col] = 0
-
-    prediction = 10 ** model.predict(query)[0]          # Prediction the result
+    
+    prediction = exp(model.predict(query)[0])           # Prediction the result
                                                         # which is initially an ndarray
                                                         # thus the use of [0]
-    print(prediction)
+    
     return flask.jsonify({"prediction":prediction})     # Return prediction in a json format
 
 if (__name__=="__main__"):
